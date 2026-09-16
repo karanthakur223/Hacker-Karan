eb0ca9996e99b7223054bb511cd5ef4738a160e6
karanthakur223 <karanupendra223@gmail.com>
2026-09-16 12:23:54 +0530

    Add files via upload

---
+++ karan_ascii_loop.sh
@@ -0,0 +1,166 @@
+#!/bin/sh
+# Generated from the uploaded 53-frame GIF. Standard ANSI terminal version.
+set -eu
+printf "\033[2J\033[?25l"
+trap 'printf "\033[0m\033[?25h\n"; exit 0' INT TERM EXIT
+F0='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F1='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F2='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F3='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F4='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F5='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F6='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F7='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F8='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F9='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F10='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F11='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F12='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F13='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F14='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F15='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F16='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F17='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F18='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F19='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F20='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F21='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F22='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F23='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F24='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F25='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F26='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F27='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F28='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F29='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F30='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F31='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F32='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F33='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F34='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F35='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F36='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F37='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F38='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F39='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F40='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F41='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F42='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F43='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F44='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F45='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F46='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F47='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F48='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F49='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F50='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F51='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+F52='$(printf %s CgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgo= | base64 -d)'
+while :; do
+printf "\033[H%s" "$F0"
+sleep 0.11
+printf "\033[H%s" "$F1"
+sleep 0.11
+printf "\033[H%s" "$F2"
+sleep 0.11
+printf "\033[H%s" "$F3"
+sleep 0.11
+printf "\033[H%s" "$F4"
+sleep 0.11
+printf "\033[H%s" "$F5"
+sleep 0.11
+printf "\033[H%s" "$F6"
+sleep 0.11
+printf "\033[H%s" "$F7"
+sleep 0.11
+printf "\033[H%s" "$F8"
+sleep 0.11
+printf "\033[H%s" "$F9"
+sleep 0.11
+printf "\033[H%s" "$F10"
+sleep 0.11
+printf "\033[H%s" "$F11"
+sleep 0.11
+printf "\033[H%s" "$F12"
+sleep 0.11
+printf "\033[H%s" "$F13"
+sleep 0.11
+printf "\033[H%s" "$F14"
+sleep 0.11
+printf "\033[H%s" "$F15"
+sleep 0.11
+printf "\033[H%s" "$F16"
+sleep 0.11
+printf "\033[H%s" "$F17"
+sleep 0.11
+printf "\033[H%s" "$F18"
+sleep 0.11
+printf "\033[H%s" "$F19"
+sleep 0.11
+printf "\033[H%s" "$F20"
+sleep 0.11
+printf "\033[H%s" "$F21"
+sleep 0.11
+printf "\033[H%s" "$F22"
+sleep 0.11
+printf "\033[H%s" "$F23"
+sleep 0.11
+printf "\033[H%s" "$F24"
+sleep 0.11
+printf "\033[H%s" "$F25"
+sleep 0.11
+printf "\033[H%s" "$F26"
+sleep 0.11
+printf "\033[H%s" "$F27"
+sleep 0.11
+printf "\033[H%s" "$F28"
+sleep 0.11
+printf "\033[H%s" "$F29"
+sleep 0.11
+printf "\033[H%s" "$F30"
+sleep 0.11
+printf "\033[H%s" "$F31"
+sleep 0.11
+printf "\033[H%s" "$F32"
+sleep 0.11
+printf "\033[H%s" "$F33"
+sleep 0.11
+printf "\033[H%s" "$F34"
+sleep 0.11
+printf "\033[H%s" "$F35"
+sleep 0.11
+printf "\033[H%s" "$F36"
+sleep 0.11
+printf "\033[H%s" "$F37"
+sleep 0.11
+printf "\033[H%s" "$F38"
+sleep 0.11
+printf "\033[H%s" "$F39"
+sleep 0.11
+printf "\033[H%s" "$F40"
+sleep 0.11
+printf "\033[H%s" "$F41"
+sleep 0.11
+printf "\033[H%s" "$F42"
+sleep 0.11
+printf "\033[H%s" "$F43"
+sleep 0.11
+printf "\033[H%s" "$F44"
+sleep 0.11
+printf "\033[H%s" "$F45"
+sleep 0.11
+printf "\033[H%s" "$F46"
+sleep 0.11
+printf "\033[H%s" "$F47"
+sleep 0.11
+printf "\033[H%s" "$F48"
+sleep 0.11
+printf "\033[H%s" "$F49"
+sleep 0.11
+printf "\033[H%s" "$F50"
+sleep 0.11
+printf "\033[H%s" "$F51"
+sleep 0.11
+printf "\033[H%s" "$F52"
+sleep 0.11
+done
